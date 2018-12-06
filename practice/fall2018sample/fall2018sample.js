e73099ed13efa3f666a386141b46532c7471a6bd
Jaymart Yabo (RIT Student) <jsy9739@boo-1303-04.main.ad.rit.edu>
2018-12-05 23:29:51 -0500

    js and css

--- practice/fall2018sample/fall2018sample.js
+++ practice/fall2018sample/fall2018sample.js
@@ -1,1 +1,24 @@
 /* Don't use <script> tags in a linked js file! */
+
+'use strict';
+
+// Get the button, and when the user clicks on it, gets the children and executes function
+$(".menubox").click(function () {
+    $(this).find(".submenu").toggle();
+})
+
+//AJAX
+let content = ("content1.txt");
+$("#choose-content").val(content);
+$("#content").load(content);
+
+$("#choose-content").change(function () {
+    content = $(this).val();
+    $("#content").load(content);
+});
+
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("submenu");
+}
