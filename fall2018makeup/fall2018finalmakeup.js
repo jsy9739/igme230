c951fd4d9a96ecfe692df011fe6744235fb010a9
Jaymart Yabo (RIT Student) <jsy9739@ad.rit.edu>
2018-12-13 13:15:35 -0500

    extra

--- fall2018makeup/fall2018finalmakeup.js
+++ fall2018makeup/fall2018finalmakeup.js
@@ -30,4 +30,21 @@ $("nav div").click(function () {
 //});
 //let increment =
 //
+//var incrementPlus;
+//
+//let buttonPlus  = $(".increment");
+//
+//var incrementPlus = buttonPlus.click(function() {
+//	var $n = $(this)
+//		.parent("#extracredit")
+//		.find("#currentcount");
+//	$n.val(Number($n.val())+1 );
+//});
 
+var click = 0;
+$("#currentcount").text(click);
+
+$("#increment").click(function(){
+    click++;
+    $("#currentcount").text(click);
+})
