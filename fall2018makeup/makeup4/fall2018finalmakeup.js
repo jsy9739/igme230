ed45a721b4c0bba1ea69ef2f3ddc93834e9417b4
Jaymart Yabo (RIT Student) <jsy9739@ad.rit.edu>
2018-12-13 12:15:55 -0500

    menu

--- fall2018makeup/makeup4/fall2018finalmakeup.js
+++ fall2018makeup/makeup4/fall2018finalmakeup.js
@@ -1,24 +1,34 @@
 'use strict';
 
 
+let story = ("story0.txt");
+
+$("#story").load(story);
+//
+//$(document).ready(function(){
+//    $("nav div").click(function(){
+//        $(".submenu").toggle();
+//    });
+//});
+
+
 $("nav div").click(function () {
     $(this).find(".submenu").toggle();
 });
 
-//let var = $;
+//
+////let var = $;
+//
+//$("nav div").click(function () {
+//    $(this).attr(“id”).toggle();
+//    event.trigger.id.toggle();
+//    this.id.toggle();
+//});
 
-$("nav div").click(function () {
-    $(this).attr(“id”).toggle();
-    event.trigger.id.toggle();
-    this.id.toggle();
-});
-
-$(document).ready(function(){
-     $("#story").load("story0.txt"); //load site last updated text
-});
-
-$(function(){
-    $( "#story" ).load( "story0.txt" );
-});
-
-$("#story").load("story0.txt");
+//$(document).ready(function(){
+//     $("#story").load("makeup4/story0.txt"); //load site last updated text
+//});
+//
+//$(function(){
+//    $( "#story" ).load( "makeup4/story0.txt" );
+//});
