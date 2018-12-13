b2d779d64983dde46c11c7613b0aec74fe6c4d17
Jaymart Yabo (RIT Student) <jsy9739@ad.rit.edu>
2018-12-13 12:17:06 -0500

    clean up

--- fall2018makeup/makeup4/fall2018finalmakeup.js
+++ fall2018makeup/makeup4/fall2018finalmakeup.js
@@ -4,31 +4,11 @@
 let story = ("story0.txt");
 
 $("#story").load(story);
-//
-//$(document).ready(function(){
-//    $("nav div").click(function(){
-//        $(".submenu").toggle();
-//    });
-//});
+
 
 
 $("nav div").click(function () {
     $(this).find(".submenu").toggle();
 });
 
-//
-////let var = $;
-//
-//$("nav div").click(function () {
-//    $(this).attr(“id”).toggle();
-//    event.trigger.id.toggle();
-//    this.id.toggle();
-//});
 
-//$(document).ready(function(){
-//     $("#story").load("makeup4/story0.txt"); //load site last updated text
-//});
-//
-//$(function(){
-//    $( "#story" ).load( "makeup4/story0.txt" );
-//});
